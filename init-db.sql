4ab2bc612283b4bb6fd0e21ea093d36cd8ce5f4b
MihailoVukorep <mihailo.vukorep@gmail.com>
2026-01-25 09:46:03 +0100

    don't need write users

--- init-db.sql
+++ init-db.sql
@@ -1,13 +1,3 @@
 CREATE DATABASE tourism_stakeholders;
 CREATE DATABASE tourism_purchase;
 CREATE DATABASE tourism_blog;
-
--- Create users if needed (optional)
-CREATE USER stakeholders_user WITH PASSWORD 'ftn';
-CREATE USER purchase_user WITH PASSWORD 'ftn';
-CREATE USER blog_user WITH PASSWORD 'ftn';
-
--- Grant permissions (optional)
-GRANT ALL PRIVILEGES ON DATABASE tourism_stakeholders TO stakeholders_user;
-GRANT ALL PRIVILEGES ON DATABASE tourism_purchase TO purchase_user;
-GRANT ALL PRIVILEGES ON DATABASE tourism_blog TO blog_user;
