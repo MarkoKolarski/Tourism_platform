5073bf6e71f70a304394e9219c300e76ed521d90
aleksandraa19 <aleksandrabegovic02@gmail.com>
2026-01-27 15:56:07 +0100

    adding logics for 15. funcionality

--- tour-service/tours/urls.py
+++ tour-service/tours/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('create/', views.create_tour, name='create_tour'),
     path('my/', views.get_my_tours, name='get_my_tours'),
     path('<int:tour_id>/keypoints/add/', views.add_keypoint, name='add_keypoint'),
+    path("<int:tour_id>/publish/", views.publish_tour),
+    path("<int:tour_id>/archive/", views.archive_tour),
+    path("status/<string:status>/", views.get_tours_by_status),
+    path("<int:tour_id>/activate/", views.activate_tour),
+    #path("<int:tour_id>/", views.get_tour_details),
 ]
